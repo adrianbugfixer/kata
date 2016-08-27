28113c75a856d99ac83fc666803b6471d4a07189
Adrian Bugfixer <adrian.bugfixer@gmail.com>
2016-08-27 04:25:53 +0200

    Deleted unused test class. Moved Prodcut daos.

--- supermarket/src/test/java/supermarket/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package supermarket;
-
-import java.util.ArrayList;
-
-public interface ProductDao {
-	public ArrayList<Product> getAll();
-	public boolean add(Product p);
-	public Product get(int idx);
-}
